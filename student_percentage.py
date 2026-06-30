6f42f3cadfe1ebbd9a65565ce97e69ef29ae1a55
SUYASH-27-UI <suyashsurve30@gmail.com>
2026-06-30 18:08:28 +0530

    Create student_percentage.py

---
+++ student_percentage.py
@@ -0,0 +1,9 @@
+marks1 = float(input("Enter marks of Subject 1: "))
+marks2 = float(input("Enter marks of Subject 2: "))
+marks3 = float(input("Enter marks of Subject 3: "))
+
+total = marks1 + marks2 + marks3
+percentage = total / 3
+
+print("Total Marks:", total)
+print("Percentage:", percentage)
